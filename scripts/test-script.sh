43f910137ca3cdacaca86cf94dc1398b87f188c5
Никита <72988968+nik07-developer@users.noreply.github.com>
2025-02-08 06:06:38 +0300

    Update test-script.sh

--- scripts/test-script.sh
+++ scripts/test-script.sh
@@ -36,7 +36,7 @@ step() {
 
   printf "=== Step %d: %s %s ===\n" "$step" "$operation" "$service"
 
-  docker compose "$operation" "$service"
+  sudo docker compose "$operation" "$service"
   if [[ "$operation" == "start" ]]; then
     "$path"/wait-for.sh -t 120 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active"
   fi
